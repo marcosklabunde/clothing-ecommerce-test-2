8093b26338c55df37e93b5074b7fa8664c3f87f9
marcosklabunde <m.klabunde@hotmail.com>
2022-12-29 08:43:29 -0300

    Added SIGN OUT button

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { createAuthUserWithEmail, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import './sign-up-form.styles.scss';
 import Button from "../button/button.component";
+import { UserContext } from "../../context/user.context";
 
 const defaultFormFields = {
     displayName: '',
@@ -14,6 +15,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
+
+    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -30,6 +33,7 @@ const SignUpForm = () => {
         try {
             const { user } = await createAuthUserWithEmail(email, password);
             await createUserDocumentFromAuth(user, { displayName });
+            setCurrentUser(user);
             resetFormFields();
         } catch (ex) {
             if (ex.code == 'auth/email-already-in-use') {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,9 +3,15 @@ import {Fragment, useContext} from 'react';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import {UserContext} from '../../context/user.context';
+import {signOutUser} from '../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        const res = await signOutUser();
+        setCurrentUser(null);
+    }
     
     return (
         <Fragment>
@@ -17,9 +23,18 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/auth'>
-                        SIGN IN
-                    </Link>
+                    {
+                        currentUser ? (
+                            <span className='nav-link' onClick={signOutHandler}>
+                                SIGN OUT
+                            </span>
+                        ) : (
+                            <Link className='nav-link' to='/auth'>
+                                SIGN IN
+                            </Link>
+                        )
+                    }
+                    
                 </div>
             </div>
             <Outlet/>
