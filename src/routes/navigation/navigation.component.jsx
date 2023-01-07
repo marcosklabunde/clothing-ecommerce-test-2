54faefc32b47fdadc66fe271839b8064a3162186
marcosklabunde <m.klabunde@hotmail.com>
2023-01-07 02:11:20 -0300

    Added new components: cart-dropdown, cart-icon and product-card, and new shop route

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,16 @@
-import {Outlet, Link} from 'react-router-dom';
-import {Fragment, useContext} from 'react';
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment, useContext } from 'react';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
-import {UserContext} from '../../context/user.context';
-import {signOutUser} from '../../utils/firebase/firebase.utils';
+import { UserContext } from '../../context/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../context/cart.context';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -29,8 +33,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }
-                    
+                    <CartIcon/>
                 </div>
+                {isCartOpen && <CartDropdown/>}
             </div>
             <Outlet/>
         </Fragment>
